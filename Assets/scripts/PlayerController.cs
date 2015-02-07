d3677d2a97b61863df37c9636d33ee42418ce0cc
Serialbocks <johnj.gw@gmail.com>
2015-02-07 02:14:10 -0600

    Added walking animations.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class PlayerController : MonoBehaviour 
+{
+	public float speedFactor;
 	
+	private Animator animator;
+	private bool facingRight = true;
+	
+	private bool grounded = false;
+	public Transform groundCheck;
+	private float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () 
+	{
+		// Check if player is on the ground
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		animator.SetBool("ground", grounded);
 	
+		float moveX = Input.GetAxis ("Horizontal");
+		float speed = moveX * speedFactor;
+		rigidbody2D.velocity = new Vector2(speed, 0);
+		animator.SetFloat("speed", Mathf.Abs (speed));
+		
+		if((speed > 0 && !facingRight)
+			|| (speed < 0 && facingRight))
+			Flip ();
+
+	}
+	
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
