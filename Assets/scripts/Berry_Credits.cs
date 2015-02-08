39227351e1b563054f8037c299ae485ae799d01e
jordannesley <jordannesley@yahoo.com>
2015-02-08 01:40:27 -0800

    credits

---
+++ Assets/scripts/Berry_Credits.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Berry_Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rigidbody2D.angularVelocity = 20f;
+
+	}
+}
---
+++ Assets/scripts/Scrolling_Credits.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scrolling_Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector2 ScrollVelocity = rigidbody2D.velocity;
+		ScrollVelocity.x = 0.0f;
+		ScrollVelocity.y = 2f;
+
+		rigidbody2D.velocity = ScrollVelocity;
+
+		if (rigidbody2D.position.y > 10f) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+
+
+}
