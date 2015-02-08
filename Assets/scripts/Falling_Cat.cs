3c8275602b2d4f547fb9615941779f85e8889130
jordannesley <jordannesley@yahoo.com>
2015-02-08 03:23:07 -0800

    level with falling cats

---
+++ Assets/scripts/Falling_Cat.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Falling_Cat : MonoBehaviour {
+
+	public float xboundary;
+
+	// Use this for initialization
+	void Start () {
+	
+		GetComponent<SpriteRenderer>().enabled = false;
+		rigidbody2D.gravityScale = 0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (GameObject.FindGameObjectWithTag ("Player").rigidbody2D.position.x.ToString ());
+		if (GameObject.FindGameObjectWithTag ("Player").rigidbody2D.position.x > xboundary) {
+			Debug.Log ("test");
+			GetComponent<SpriteRenderer>().enabled = true;
+			rigidbody2D.gravityScale = 5f;
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player")
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}	
+	}
+
+}
---
+++ Assets/scripts/MovingPlatform.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public int Frames = 60;
+	public float Speed;
+	private bool MovingRight = true;
+
+	private int currentframe;
+	private Vector2 InitialPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+		currentframe = Frames;
+		InitialPosition = rigidbody2D.position;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (currentframe < 0) {
+			currentframe = Frames;
+			MovingRight = !MovingRight;
+		}
+
+		Vector2 PlatformVelocity = rigidbody2D.velocity;
+		PlatformVelocity.x = Speed;
+		PlatformVelocity.y = 0f;
+
+		if (MovingRight)
+			rigidbody2D.velocity = PlatformVelocity;
+		else
+			rigidbody2D.velocity = -PlatformVelocity;
+
+		currentframe--;
+
+	}
+}
